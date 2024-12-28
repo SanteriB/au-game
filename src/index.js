bb30471c5af7f5bfe0cf15d69aaf45576daa0820
SanteriB <santeri.sbr@gmail.com>
2024-12-28 14:08:36 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { Game } from "./Game";
+
+const game = new Game();
+console.log(game);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: { 
+        filename: "bundle.js", 
+        path: path.resolve(__dirname, "dist"), 
+    },
+    devServer: { 
+        static: { 
+            directory: path.join(__dirname, 'dist'),
+        }, 
+        compress: true, 
+        port: 8000,
+        open: true,
+    },
+    plugins: [ 
+        new HtmlWebpackPlugin({ 
+            template: './src/index.html',
+        }), 
+    ],
+    module: { 
+        rules: [ { 
+            test: /\.js$/, 
+            exclude: /node_modules/, 
+            use: { loader: "babel-loader", }, 
+        }, ], 
+    }, 
+};
